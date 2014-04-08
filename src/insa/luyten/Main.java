ed318f1e5c8201011632012b1f7174cca369b401
Gwenegan Hudin <gwenegan.hudin@insa-rennes.fr>
2014-04-09 00:34:26 +0200

    Simple demo index and query on src
    First try at IntelliJIDEA
    Basic lucene libs import and use in main, just to get started.
    To try it, add the needed libs in $PROJECT_DIR$/lucene_jar

---
+++ src/insa/luyten/Main.java
@@ -0,0 +1,19 @@
+package insa.luyten;
+
+import org.apache.lucene.demo.IndexFiles;
+import org.apache.lucene.demo.SearchFiles;
+
+/**
+ * Created by Gweylorth on 08/04/2014.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        IndexFiles.main(new String[]{"-docs", "C:\\Users\\Gwaihir\\Downloads\\lucene-4.7.1-src\\lucene-4.7.1\\demo\\src"});
+        try {
+            SearchFiles.main(new String[]{});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
